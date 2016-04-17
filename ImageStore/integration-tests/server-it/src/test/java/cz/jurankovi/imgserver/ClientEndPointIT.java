07b303c619c5b7082270f1249224a611b364e45e
Vojtech Juranek <vojtech.juranek@gmail.com>
2016-04-18 00:59:23 +0200

    Prepare server integration tests

---
+++ ImageStore/integration-tests/server-it/src/test/java/cz/jurankovi/imgserver/ClientEndPointIT.java
@@ -0,0 +1,40 @@
+package cz.jurankovi.imgserver;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+
+public class ClientEndPointIT {
+
+    private static final String CLIENT_VERSION_URL = "http://localhost:8080/server/rest/client/version";
+
+    @Test
+    public void testClientVersion() throws Exception {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(getClientVersionStream(CLIENT_VERSION_URL));
+
+        NodeList clientVerXml = doc.getElementsByTagName("clientVersion");
+        assertEquals(1, clientVerXml.getLength());
+        NamedNodeMap attrs = clientVerXml.item(0).getAttributes();
+        assertEquals(0, Short.valueOf(attrs.getNamedItem("major").getTextContent()).shortValue());
+        assertEquals(1, Short.valueOf(attrs.getNamedItem("minor").getTextContent()).shortValue());
+    }
+
+    public static InputStream getClientVersionStream(String clientVersionUrl) throws Exception {
+        URL url = new URL(clientVersionUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        return conn.getInputStream();
+    }
+}
