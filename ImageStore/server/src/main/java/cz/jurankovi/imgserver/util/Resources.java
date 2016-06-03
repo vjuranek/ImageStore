a59f105218cd484c3516781758d906c871ff4367
Vojtech Juranek <vojtech.juranek@gmail.com>
2016-06-03 22:12:36 +0200

    Add logger provider

--- ImageStore/server/src/main/java/cz/jurankovi/imgserver/util/Resources.java
+++ ImageStore/server/src/main/java/cz/jurankovi/imgserver/util/Resources.java
@@ -1,12 +1,20 @@
 package cz.jurankovi.imgserver.util;
 
+import java.util.logging.Logger;
+
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 public class Resources {
-    
+
     @Produces
     @PersistenceContext(unitName = "imgserver")
     private EntityManager em;
+
+    @Produces
+    public Logger produceLog(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
 }
